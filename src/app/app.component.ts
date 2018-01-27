fc37a2390d8d1898d3766f7f3e2c79613af5f017
Hesham Elkhatib <hesham.elkhatib92@gmail.com>
2018-01-27 14:03:06 +0200

    Add hardcoded attributes to app commponent Add isLocalActive attribute to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'store-finder';
+  // used to set the active tab
+  isLocalActive = true;
+  //if this wasn't for a predefined product, these values should be read from the route
+  productEan = "00000010000038";
+  country = "DE";
+  language = "de";
 }
