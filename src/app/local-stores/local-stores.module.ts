8e953d27d733fe4437d7853ad04b2fc76f3b20ef
Hesham Elkhatib <hesham.elkhatib92@gmail.com>
2018-01-27 14:21:21 +0200

    Move local-stores service to the local-stores directory

--- src/app/local-stores/local-stores.module.ts
+++ src/app/local-stores/local-stores.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LocalStoresComponent } from './local-stores.component';
-import { LocalStoresService } from '../local-stores.service';
+import { LocalStoresService } from './local-stores.service';
 
 @NgModule({
   imports: [
