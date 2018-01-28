6e3846b212434eef87e41448fcbde3dfc93cb20b
Hesham Elkhatib <hesham.elkhatib92@gmail.com>
2018-01-28 07:50:07 +0200

    Handle Search event Use LocalStoresService in LocalStoreFinder component

--- src/app/local-stores/local-store-finder.component.ts
+++ src/app/local-stores/local-store-finder.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+import { OnChanges } from '@angular/core/src/metadata/lifecycle_hooks';
+import { LocalStoresService } from './local-stores.service';
+import { LocalStore } from './local-store.model';
 
 @Component({
   selector: 'local-store-finder',
@@ -6,10 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./local-store-finder.component.css']
 })
 export class LocalStoreFinderComponent implements OnInit {
+  // inputs
+  @Input()
+  productEan: string;
+  @Input()
+  country: string;
+  @Input()
+  language: string;
+  //binded to the search control
+  searchText: string;
 
-  constructor() { }
+  localStores: Array<LocalStore> = [];
+  centerLat: number = 51.678418;
+  centerLng: number = 7.809007;
+
+  constructor(private localStoresService: LocalStoresService) { }
 
   ngOnInit() {
+  }
+  
+  onSearch(): void{
+    this.localStoresService.getByEanAndCity(this.country, this.language, this.productEan, this.searchText, 0, 1000).
+    pipe(
+      map(result => result.resultGroups),
+      map(groups => groups[0]),
+      map(group => group.records),
+    ).
+    subscribe((localStores: Array<LocalStore>) => {
+      this.localStores = localStores;
+      this.centerLng = localStores[0].geoInfo.lng;
+      this.centerLat = localStores[0].geoInfo.lat;
+      console.log(this.localStores);
+    });
+    
   }
 
 }
