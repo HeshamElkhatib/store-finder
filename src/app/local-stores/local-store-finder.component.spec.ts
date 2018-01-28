0753e28e1bcdf0f126e8e3213fcaccd2463f6bc0
Hesham Elkhatib <hesham.elkhatib92@gmail.com>
2018-01-28 07:51:24 +0200

    Add search layer styling

--- src/app/local-stores/local-store-finder.component.spec.ts
+++ src/app/local-stores/local-store-finder.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LocalStoreFinderComponent } from './local-store-finder.component';
 
-describe('LocalStoresComponent', () => {
+describe('LocalStoreFinderComponent', () => {
   let component: LocalStoreFinderComponent;
   let fixture: ComponentFixture<LocalStoreFinderComponent>;
 
