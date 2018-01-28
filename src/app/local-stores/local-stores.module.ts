f0057e8e6d15234cb15d390509e8a284688bf908
Hesham Elkhatib <hesham.elkhatib92@gmail.com>
2018-01-28 07:49:05 +0200

    Add FormsModule to local-stores module

--- src/app/local-stores/local-stores.module.ts
+++ src/app/local-stores/local-stores.module.ts
@@ -1,12 +1,15 @@
 import { NgModule, } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+
 import { LocalStoreFinderComponent } from './local-store-finder.component';
 import { LocalStoresService } from './local-stores.service';
 import { AgmCoreModule } from "@agm/core"
 import { environment } from '../../environments/environment';
 @NgModule({
   imports: [
+    FormsModule,
     CommonModule,
     HttpClientModule,
     AgmCoreModule.forRoot({
