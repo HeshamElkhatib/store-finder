88b2b7197df011891257d73afb85547bedb4d07b
Hesham Elkhatib <hesham.elkhatib92@gmail.com>
2018-01-29 05:50:53 +0200

    Add tokens variables to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  commerceConnectorApiToken: '1e7069cb5f45d06fb0222c45474294034b3c306d',
+  googlMapsApiKey: 'AIzaSyCq_VH60lCjrr7MORAEfBRUzj1AF2friIw'
 };
